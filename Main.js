78024b3a644e334c9c406bdaab7cd4c5b301d5b1
ARNOLD <146892654+OMGSAM@users.noreply.github.com>
2024-12-06 17:19:12 +0100

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,93 @@
+ 
+
+# 1. Quelle est la caractéristique principale de DevOps ?
+reponse_1 = "a. Automatisation et supervision des étapes de création logicielle"
+
+# 2. Que signifie le terme "DevOps" ?
+reponse_2 = "b. Développement et Opérations"
+
+# 3. Le DevOps vise principalement à :
+reponse_3 = "b. Accélérer le déploiement tout en améliorant la qualité logicielle"
+
+# 4. Quelle est la définition principale de DevOps ?
+reponse_4 = "b. Une méthodologie pour accélérer les cycles de développement et d'exploitation"
+
+# 5. Que se passe-t-il durant la phase de conception dans un cycle DevOps ?
+reponse_5 = "c. Les développeurs collectent les exigences du client"
+
+# 6. Lequel des éléments suivants est un objectif de DevOps ?
+reponse_6 = "c. Améliorer la communication entre développement et exploitation"
+
+# 7. Qu’est-ce que représente le symbole d’infini utilisé pour la chaîne DevOps ?
+reponse_7 = "a. Le cycle de vie itératif et continu"
+
+# 8. Qu’est-ce que l’intégration continue (CI) ?
+reponse_8 = "b. Livraison automatisée de modifications testées unitairement"
+
+# 9. Quelle étape fait partie du déploiement continu (CD) ?
+reponse_9 = "c. Gestion automatique des backups avant déploiement"
+
+# 10. En quoi DevOps améliore-t-il la satisfaction client ?
+reponse_10 = "b. En introduisant des logiciels de meilleure qualité plus rapidement"
+
+# 11. Quel est un des principaux avantages de DevOps en termes de satisfaction client ?
+reponse_11 = "b. Amélioration de l’expérience utilisateur grâce à des logiciels de meilleure qualité"
+
+# 12. Qu’est-ce que le "Time-to-Market" (TTM) ?
+reponse_12 = "b. Le temps écoulé entre la conception et la mise en production d’un produit"
+
+# 13. Qu'est-ce que GitLab CI/CD permet d'automatiser ?
+reponse_13 = "b. Les builds, les tests, les livraisons et les déploiements"
+
+# 14. Dans un pipeline GitLab, que contient une étape (stage) ?
+reponse_14 = "c. Des tâches (jobs)"
+
+# 15. Qu’est-ce qu’un runner dans GitLab CI/CD ?
+reponse_15 = "b. Une application qui exécute les tâches (jobs) du pipeline"
+
+# 16. Qu’arrive-t-il si une tâche (job) échoue dans un stage ?
+reponse_16 = "c. L’ensemble du pipeline échoue sauf dans des cas particuliers"
+
+# 17. Quels fichiers sont produits par les pipelines et utilisés pour visualiser la couverture de tests ?
+reponse_17 = "b. Des artefacts"
+
+# 18. Qu’est-ce qu’un pipeline CI/CD ?
+reponse_18 = "a. Une série d'étapes à réaliser pour distribuer une nouvelle version d'un logiciel"
+
+# 19. Quelle phase n’appartient pas aux étapes courantes d’un pipeline CI/CD ?
+reponse_19 = "b. Distribution"
+
+# 20. Dans un pipeline GitLab CI/CD, les tâches d'une même étape :
+reponse_20 = "b. Sont exécutées en parallèle"
+
+# 21. Quelle est l’utilité principale du fichier .gitlab-ci.yml ?
+reponse_21 = "b. Configurer les stages et les jobs d’un pipeline"
+
+# 22. Quels sont les avantages principaux d’un pipeline CI/CD ?
+reponse_22 = "a. Gagner en rapidité, réduire les erreurs manuelles et améliorer la fiabilité"
+
+# 23. Quel outil est un exemple de gestion de code source ?
+reponse_23 = "c. Git"
+
+# 24. Quel outil permet de gérer les conteneurs pour déployer des applications isolées ?
+reponse_24 = "b. Docker"
+
+# 25. Qu'est-ce que DevOps ?
+reponse_25 = "b. Une philosophie visant à intégrer le développement et l'exploitation"
+
+# 26. Quels sont les composants clés de Devops ?
+reponse_26 = "d. Toutes les réponses"
+
+# 27. Objectifs principaux de l'intégration continue dans le cadre de DevOps ?
+reponse_27 = "d. Toutes les réponses"
+
+# 28. La livraison continue dans DevOps implique :
+reponse_28 = "a. Mettre en production chaque modification de code dès qu'elle est prête"
+
+# 29. Quel est le rôle d'un pipeline d'intégration continue dans DevOps ?
+reponse_29 = "b. Automatiser les tests et le déploiement"
+
+# 30. Qu'est-ce que GitLab Runner ?
+reponse_30 = "c. Un agent d'exécution pour les jobs CI/CD"
+
+ 
